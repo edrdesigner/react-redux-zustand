8364ee5077c75dbe6581230045ff1ca34df2a633
eduardo.reichert <eduardo.reichert@totvs.com.br>
2023-07-20 20:51:02 -0300

    chore: application with redux

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { useCurrentLesson } from '../store/slices/player';
+
 export function Header() {
+  const { currentModule, currentLesson } = useCurrentLesson();
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+      <h1 className="text-2xl font-bold">{currentModule.title}</h1>
       <span className="text-sm text-zinc-400">
-        Modulo desvandando o Redux na pratica
+        Modulo "{currentLesson.title}"
       </span>
     </div>
   );
