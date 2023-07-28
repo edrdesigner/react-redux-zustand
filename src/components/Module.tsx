bead4e5d50bbba8b5a245b3cc3382271287484fb
eduardo.reichert <eduardo.reichert@totvs.com.br>
2023-07-28 17:43:48 -0300

    chore: finish zustand store with tests

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,9 +1,7 @@
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from 'lucide-react';
-import { useDispatch } from 'react-redux';
+import { useStore } from '../zustand-store';
 import { Lesson } from './Lesson';
-import { useAppDispatch, useAppSelector } from '../store';
-import { play } from '../store/slices/player';
 
 interface ModuleProps {
   title: string;
@@ -12,20 +10,16 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const dispatch = useAppDispatch();
-
-  const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
-    const { currentModuleIndex, currentLessonIndex } = state.player;
-
-    return {
-      currentModuleIndex,
-      currentLessonIndex,
-    };
-  });
-
-  const lessons = useAppSelector((state) => {
-    return state.player.course?.modules[moduleIndex].lessons;
-  });
+  const { currentModuleIndex, currentLessonIndex, play, lessons } = useStore(
+    (store) => {
+      return {
+        lessons: store?.course?.modules[moduleIndex].lessons,
+        currentModuleIndex: store.currentModuleIndex,
+        currentLessonIndex: store.currentLessonIndex,
+        play: store.play,
+      };
+    }
+  );
 
   return (
     <Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
@@ -42,21 +36,22 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons && lessons.map((lesson, lessonIndex) => {
-            const isCurrent =
-              currentModuleIndex === moduleIndex &&
-              currentLessonIndex === lessonIndex;
+          {lessons &&
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex === moduleIndex &&
+                currentLessonIndex === lessonIndex;
 
-            return (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                duration={lesson.duration}
-                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-                isCurrent={isCurrent}
-              />
-            );
-          })}
+              return (
+                <Lesson
+                  key={lesson.id}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  onPlay={() => play([moduleIndex, lessonIndex])}
+                  isCurrent={isCurrent}
+                />
+              );
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -4,21 +4,20 @@ import { MessageCircle } from 'lucide-react';
 import { Header } from '../components/Header';
 import { Video } from '../components/Video';
 import { Module } from '../components/Module';
-import { useAppDispatch, useAppSelector } from '../store';
-import { loadCourse, useCurrentLesson } from '../store/slices/player';
+import { useCurrentLesson, useStore } from '../zustand-store';
 
 export function Player() {
-  const dispatch = useAppDispatch();
-
-  const modules = useAppSelector((state) => {
-    return state.player.course?.modules;
+  const { course, load } = useStore((store) => {
+    return {
+      course: store.course,
+      load: store.load,
+    };
   });
-
   const { currentLesson } = useCurrentLesson();
 
   useEffect(() => {
-    void dispatch(loadCourse());
-  }, []);
+    void load();
+  }, [load]);
 
   useEffect(() => {
     if (currentLesson) {
@@ -44,8 +43,8 @@ export function Player() {
           </div>
 
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll divide-y-2 divide-zinc-900 scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            {modules &&
-              modules.map((module, index) => {
+            {course?.modules &&
+              course.modules.map((module, index) => {
                 return (
                   <Module
                     key={module.id}
